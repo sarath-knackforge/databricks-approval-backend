c966f2346e8afda9fc990a81b2f10e3f99f23adf
sarathkumar@kanckforge <sarathkumar.r@knackforge.com>
2025-11-24 14:31:32 +0530

    Update service account JSON file

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ SCOPE = [
 
 # Load your service account file (must be included in Render project)
 creds = ServiceAccountCredentials.from_json_keyfile_name(
-    "service_account.json", SCOPE
+    "gen-lang-client-0000776926-fae8bb8259b2.json", SCOPE
 )
 
 client = gspread.authorize(creds)
